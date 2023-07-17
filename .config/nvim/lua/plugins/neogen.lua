96c9d6b9b0eb3e294f00adf1ab9f9082aba9e8cb
Adam Chhina <amanschhina@gmail.com>
2023-07-17 02:44:49 -0400

    Added annotation generator.

---
+++ .config/nvim/lua/plugins/neogen.lua
@@ -0,0 +1,17 @@
+return {
+  "danymat/neogen",
+  dependencies = "nvim-treesitter/nvim-treesitter",
+  config = true,
+  opts = {
+    snippet_engine = "luasnip",
+    languages = {
+      python = {
+        template = {
+          annotation_convention = "numpydoc"
+        }
+      }
+    }
+  },
+  -- Follow stable versions only
+  version = "*"
+}
