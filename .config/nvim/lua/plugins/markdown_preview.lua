b3b3e805a7c66dd19573c35f09595e17c97464ba
Adam Chhina <amanschhina@gmail.com>
2023-08-06 14:37:40 -0400

    Added markdown preview.

---
+++ .config/nvim/lua/plugins/markdown_preview.lua
@@ -0,0 +1,6 @@
+return {
+    'iamcco/markdown-preview.nvim',
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" , "md"},
+    build = function() vim.fn["mkdp#util#install"]() end,
+}
