a41672660ab04a2d4a12942eb2827783dc5f5ef7
Adam Chhina <amanschhina@gmail.com>
2023-11-23 18:14:30 -0500

    Add aliases in a separate vc file.

---
+++ .config/bash/aliases.sh
@@ -0,0 +1,23 @@
+##### Editors #####
+
+alias v="nvim"
+alias vi="nvim" 
+
+##### Python #####
+
+alias pip="python3 -m pip"
+alias pip3="pip"
+
+##### Shell Utils #####
+
+# exa - https://the.exa.website/
+if where exa &> /dev/null; then
+    alias ls="exa --header"
+    alias lst="exa --tree"
+    alias lsg="exa --header --long --git"
+fi
+
+##### Custom #####
+
+# Manage dotfiles with bare clone
+alias config="/usr/bin/git --git-dir=${HOME}/.cfg/ --work-tree=${HOME}"
