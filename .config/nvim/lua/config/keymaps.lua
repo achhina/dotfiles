bb6da94ee2ed5c545dbd54d6a9f75d65fff5e547
Adam Chhina <amanschhina@gmail.com>
2024-01-03 01:25:16 -0500

    Removed leader hjkl keymaps for window switching for in favor of <C-[hjkl]> via vim-tmux-navigator.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -14,10 +14,6 @@ function M.load_keymaps()
     -- Window splitting and management keymaps
     vim.keymap.set('n', '<leader>S', ':split<CR>', { noremap = true, silent = true})
     vim.keymap.set('n', '<leader>V', ':vsplit<CR>', { noremap = true, silent = true})
-    vim.keymap.set('n', '<leader>j', '<C-w>j', { noremap = true, silent = true})
-    vim.keymap.set('n', '<leader>k', '<C-w>k', { noremap = true, silent = true})
-    vim.keymap.set('n', '<leader>h', '<C-w>h', { noremap = true, silent = true})
-    vim.keymap.set('n', '<leader>l', '<C-w>l', { noremap = true, silent = true})
 end
 
 return M
