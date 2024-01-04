d703804e470dc0c95c72f75856261068e6be1604
Adam Chhina <amanschhina@gmail.com>
2024-01-04 00:44:05 -0500

    Trying out night-owl theme.

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,7 +1,19 @@
-return {
+local rose_pine = {
     'rose-pine/neovim',
+    lazy = false,
     priority = 1000,
     config = function()
       vim.cmd.colorscheme 'rose-pine'
     end,
 }
+
+local night_owl = {
+    'oxfist/night-owl.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'night-owl'
+    end,
+}
+
+return night_owl
