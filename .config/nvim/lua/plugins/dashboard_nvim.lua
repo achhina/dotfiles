a1e705f2c5d07da12d1eeadd1a7dc878e5bec234
Adam Chhina <amanschhina@gmail.com>
2024-01-07 22:37:41 -0500

    Changed dashboard header and added dep.

--- .config/nvim/lua/plugins/dashboard_nvim.lua
+++ .config/nvim/lua/plugins/dashboard_nvim.lua
@@ -2,8 +2,12 @@ return {
     "nvimdev/dashboard-nvim",
     event = "VimEnter",
     config = function()
+        local ascii = require('ascii')
         require("dashboard").setup {
-            theme = "hyper"
+            theme = "hyper",
+            config = {
+                header = ascii.art.misc.krakens.sleekraken
+            }
         }
     end,
     dependencies = { { "nvim-tree/nvim-web-devicons" } }
