98a990f36c1067c7a2bdf34642d3918e7a88e998
Adam Chhina <amanschhina@gmail.com>
2024-02-07 04:39:23 -0500

    Show hidden files in file tree.

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,6 +1,6 @@
 return {
 	"stevearc/oil.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {},
+	opts = { view_options = { show_hidden = true } },
 	keys = { { "-", mode = "n", "<CMD>Oil<CR>", desc = "Open parent directory" } },
 }
