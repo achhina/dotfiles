89b09c6156f98d23d4805607ac87adb3d5654f6b
Adam Chhina <amanschhina@gmail.com>
2024-02-07 04:40:44 -0500

    Dark mode for MarkdownPreview.

--- nvim/lua/plugins/markdown_preview.lua
+++ nvim/lua/plugins/markdown_preview.lua
@@ -2,6 +2,9 @@ return {
 	"iamcco/markdown-preview.nvim",
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 	ft = { "markdown", "md" },
+	config = function()
+		vim.g.mkdp_theme = "dark"
+	end,
 	build = function()
 		vim.fn["mkdp#util#install"]()
 	end,
