30cb39e2dd9cc8b53a67045fb5c74e784c729a19
Adam Chhina <amanschhina@gmail.com>
2024-07-31 07:46:58 -0400

    Go to last edit when opening file.

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -20,6 +20,16 @@ function M.load_autocmd()
 		end,
 		pattern = table.concat(ephemeral_buffers, ","),
 	})
+
+	-- Go to last edit position when opening file
+	vim.api.nvim_create_autocmd("BufReadPost", {
+		callback = function()
+			local mark = vim.api.nvim_buf_get_mark(0, '"')
+			if mark[1] > 1 and mark[1] <= vim.api.nvim_buf_line_count(0) then
+				vim.api.nvim_win_set_cursor(0, mark)
+			end
+		end,
+	})
 end
 
 return M
