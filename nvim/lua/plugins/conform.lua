97f7bea2fd1a6b0c3999905e1d3ecba0318a9887
Adam Chhina <amanschhina@gmail.com>
2024-09-15 20:00:55 -0400

    Add ruff lint/format

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,17 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {
+		lsp_fallback = true,
+		formatters_by_ft = {
+			lua = { "stylua", lsp_fallback = true },
+			python = { "ruff_format", "ruff_fix", "ruff_organize_imports" },
+			rust = { "rustfmt" },
+			html = { "prettier" },
+			css = { "prettier" },
+			js = { "prettier" },
+			jsx = { "prettier" },
+		},
+		notify_on_error = true,
+		log_level = vim.log.levels.DEBUG,
+	},
+}
