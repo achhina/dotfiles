491681044979df4d6350ad2eea16500d86cd4db0
Adam Chhina <amanschhina@gmail.com>
2024-09-25 00:38:56 -0400

    Something something can't load in config because of Lazyvim so I set it for each formatter, something

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -3,14 +3,46 @@ return {
 	opts = {
 		lsp_fallback = true,
 		formatters_by_ft = {
-			lua = { "stylua" },
-			python = { "ruff_format", "ruff_fix", "ruff_organize_imports" },
-			rust = { "rustfmt" },
-			html = { "prettier" },
-			css = { "prettier" },
-			js = { "prettier" },
-			jsx = { "prettier" },
+			lua = {
+				"stylua",
+				format_on_save = nil,
+				format_after_save = nil,
+			},
+			python = {
+				"ruff_format",
+				"ruff_fix",
+				"ruff_organize_imports",
+				format_on_save = nil,
+				format_after_save = nil,
+			},
+			rust = {
+				"rustfmt",
+				format_on_save = nil,
+				format_after_save = nil,
+			},
+			html = {
+				"prettier",
+				format_on_save = nil,
+				format_after_save = nil,
+			},
+			css = {
+				"prettier",
+				format_on_save = nil,
+				format_after_save = nil,
+			},
+			js = {
+				"prettier",
+				format_on_save = nil,
+				format_after_save = nil,
+			},
+			jsx = {
+				"prettier",
+				format_on_save = nil,
+				format_after_save = nil,
+			},
 		},
 		notify_on_error = true,
+		format_on_save = nil,
+		format_after_save = nil,
 	},
 }
