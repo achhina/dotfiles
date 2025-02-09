eca323c5305534fab8e5364563e25df5284be824
Adam Chhina <amanschhina@gmail.com>
2025-02-08 22:15:19 -0500

    Disable keymaps that interfere with vim/tmux navigation

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,6 +1,20 @@
 return {
 	"stevearc/oil.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = { view_options = { show_hidden = true } },
-	keys = { { "-", mode = "n", "<CMD>Oil<CR>", desc = "Open parent directory" } },
+	---@module 'oil'
+	---@type oil.SetupOpts
+	-- Optional dependencies
+	dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+	lazy = false,
+	config = function()
+		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+		-- Disable keymaps related to vim/tmux navigation
+		local oil = require("oil")
+		oil.setup({
+			keymaps = {
+				["<C-h>"] = false,
+				["<C-l>"] = false,
+			},
+		})
+	end,
 }
