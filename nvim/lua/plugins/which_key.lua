6c1b3919acae798ec11baad85728f058095f6f3f
Adam Chhina <amanschhina@gmail.com>
2025-02-17 16:57:53 -0500

    Use updated config for `which-key` including it as a "VeryLazy" event and adding in the keymap for buffer local keys.

--- nvim/lua/plugins/which_key.lua
+++ nvim/lua/plugins/which_key.lua
@@ -1,6 +1,15 @@
 return {
-	-- Useful plugin to show you pending keybinds.
 	"folke/which-key.nvim",
 	dependencies = { "echasnovski/mini.icons" },
+	event = "VeryLazy",
 	opts = {},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
 }
