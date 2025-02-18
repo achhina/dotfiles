5353a6757f147d89a0f73650420f4f56fb88bdcd
Adam Chhina <amanschhina@gmail.com>
2025-02-17 21:34:47 -0500

    Removed dashboard.nvim in favor of dashboard from snacks.nvim.

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,11 +1,17 @@
 return {
 	"folke/snacks.nvim",
+	dependencies = {
+		-- Used for dashboard ascii art
+		"MaximilianLloyd/ascii.nvim",
+	},
 	priority = 1000,
 	lazy = false,
 	---@type snacks.Config
 	opts = {
 		bigfile = { enabled = false },
-		dashboard = { enabled = false },
+		dashboard = {
+			enabled = true,
+		},
 		explorer = { enabled = false },
 		indent = { enabled = false },
 		input = { enabled = false },
@@ -17,4 +23,14 @@ return {
 		statuscolumn = { enabled = false },
 		words = { enabled = false },
 	},
+	config = function()
+		local opts = {
+			dashboard = {
+				preset = {
+					header = table.concat(require("ascii.art").misc.krakens.sleekraken, "\n"),
+				},
+			},
+		}
+		require("snacks").setup(opts)
+	end,
 }
