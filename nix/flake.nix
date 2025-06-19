b505feb1e786b889a0799a45c233651139530468
Adam Chhina <amanschhina@gmail.com>
2025-06-19 17:44:49 -0400

    Use mac-app-util to trampoline app contents and permissions into expected Apps location in MacOS so permissions aren't reset after every upgrade

---
+++ nix/flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Home Manager configuration for achhina";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    mac-app-util = {
+      url = "github:hraban/mac-app-util";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, mac-app-util }:
+    let
+      # Automatically detect system architecture
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Function to create home configuration for any system
+      mkHomeConfiguration = system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          # Only include mac-app-util on Darwin systems
+          modules = [
+            ./home-manager/home.nix
+          ] ++ nixpkgs.lib.optionals (nixpkgs.lib.hasSuffix "darwin" system) [
+            mac-app-util.homeManagerModules.default
+          ];
+        in
+        home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          inherit modules;
+        };
+    in {
+      homeConfigurations = forAllSystems (system: {
+        achhina = mkHomeConfiguration system;
+      });
+
+      # Convenience outputs for current system
+      homeConfigurations.achhina = mkHomeConfiguration (builtins.currentSystem or "aarch64-darwin");
+    };
+}
