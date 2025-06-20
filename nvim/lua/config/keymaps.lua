eed85149dfd34dfe8bf407e50345e8de571b1a63
Adam Chhina <amanschhina@gmail.com>
2025-06-20 19:48:37 -0400

    Enhance keymap organization with comprehensive shortcuts and better structure
    - Add helper function for cleaner keymap creation with sensible defaults
    - Implement comprehensive window management and navigation
    - Add buffer management with navigation, deletion, and organization
    - Add tab management for better workspace organization
    - Implement line/selection movement with Alt+j/k shortcuts
    - Add better indenting that preserves visual selection
    - Add system clipboard integration for copy/paste operations
    - Add quickfix and location list navigation
    - Implement better command line and insert mode editing
    - Add file operations for save, quit, new, reload
    - Add text manipulation shortcuts with cursor positioning
    - Add screen centering on navigation for better UX
    - Add number increment/decrement shortcuts
    - Add word replacement functionality
    - Add file execution permissions toggle
    - Add various toggle options for editor settings
    - Enhance macro shortcuts for better workflow
    - Organize all keymaps with clear categories and descriptions
    - Add better paste behavior that doesn't overwrite registers
    - Add delete to black hole register functionality

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,22 +1,168 @@
 local M = {}
 
+-- Helper function for easier keymap creation
+local function map(mode, lhs, rhs, opts)
+	opts = opts or {}
+	opts.silent = opts.silent ~= false
+	vim.keymap.set(mode, lhs, rhs, opts)
+end
+
 function M.load_keymaps()
 	-- [[ Basic Keymaps ]]
 
-	-- Keymaps for better default experience
-	-- See `:help vim.keymap.set()`
-	vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+	-- Disable space in normal and visual mode (leader key)
+	map({ "n", "v" }, "<Space>", "<Nop>")
 
-	-- Remap for dealing with word wrap
-	vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-	vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+	-- Better navigation with word wrap
+	map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
+	map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
 
-	-- Window splitting and management keymaps
-	vim.keymap.set("n", "<leader>S", ":split<CR>", { noremap = true, silent = true })
-	vim.keymap.set("n", "<leader>V", ":vsplit<CR>", { noremap = true, silent = true })
+	-- Clear search highlighting
+	map("n", "<Esc>", "<cmd>nohlsearch<CR>")
+	map("n", "<leader>/", "<cmd>nohlsearch<CR>", { desc = "Clear search highlights" })
 
-	-- Play macro in q register
-	vim.keymap.set("n", "@q", "Q", { noremap = true, silent = true })
+	-- Better indenting
+	map("v", "<", "<gv", { desc = "Indent left and reselect" })
+	map("v", ">", ">gv", { desc = "Indent right and reselect" })
+
+	-- Move lines up/down
+	map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move line down" })
+	map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move line up" })
+	map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move line down" })
+	map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move line up" })
+	map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move selection down" })
+	map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move selection up" })
+
+	-- Better paste (don't overwrite register)
+	map("v", "p", '"_dP', { desc = "Paste without overwriting register" })
+
+	-- Delete to black hole register
+	map({ "n", "v" }, "<leader>d", '"_d', { desc = "Delete to black hole register" })
+
+	-- Copy to system clipboard
+	map({ "n", "v" }, "<leader>y", '"+y', { desc = "Copy to system clipboard" })
+	map("n", "<leader>Y", '"+Y', { desc = "Copy line to system clipboard" })
+
+	-- Paste from system clipboard
+	map({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from system clipboard" })
+	map({ "n", "v" }, "<leader>P", '"+P', { desc = "Paste before from system clipboard" })
+
+	-- Window management
+	map("n", "<leader>wv", "<C-w>v", { desc = "Split window vertically" })
+	map("n", "<leader>wh", "<C-w>s", { desc = "Split window horizontally" })
+	map("n", "<leader>we", "<C-w>=", { desc = "Make windows equal size" })
+	map("n", "<leader>wx", "<cmd>close<CR>", { desc = "Close current window" })
+	map("n", "<leader>wm", "<cmd>MaximizerToggle<CR>", { desc = "Toggle window maximize" })
+
+	-- Window navigation
+	map("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
+	map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
+	map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
+	map("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
+
+	-- Resize windows
+	map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+	map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+	map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+	map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+
+	-- Buffer management
+	map("n", "<leader>bd", "<cmd>bdelete<CR>", { desc = "Delete buffer" })
+	map("n", "<leader>bD", "<cmd>bdelete!<CR>", { desc = "Force delete buffer" })
+	map("n", "<leader>ba", "<cmd>%bdelete|edit#|bdelete#<CR>", { desc = "Delete all buffers but current" })
+	map("n", "<leader>bl", "<cmd>blast<CR>", { desc = "Go to last buffer" })
+	map("n", "<leader>bf", "<cmd>bfirst<CR>", { desc = "Go to first buffer" })
+
+	-- Buffer navigation
+	map("n", "[b", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
+	map("n", "]b", "<cmd>bnext<cr>", { desc = "Next buffer" })
+	map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
+	map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+
+	-- Tab management
+	map("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+	map("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+	map("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+	map("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+	map("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+
+	-- Quickfix list
+	map("n", "[q", "<cmd>cprevious<CR>", { desc = "Previous quickfix item" })
+	map("n", "]q", "<cmd>cnext<CR>", { desc = "Next quickfix item" })
+	map("n", "<leader>qo", "<cmd>copen<CR>", { desc = "Open quickfix list" })
+	map("n", "<leader>qc", "<cmd>cclose<CR>", { desc = "Close quickfix list" })
+
+	-- Location list
+	map("n", "[l", "<cmd>lprevious<CR>", { desc = "Previous location item" })
+	map("n", "]l", "<cmd>lnext<CR>", { desc = "Next location item" })
+	map("n", "<leader>lo", "<cmd>lopen<CR>", { desc = "Open location list" })
+	map("n", "<leader>lc", "<cmd>lclose<CR>", { desc = "Close location list" })
+
+	-- Better command line editing
+	map("c", "<C-a>", "<Home>", { desc = "Go to beginning of line" })
+	map("c", "<C-e>", "<End>", { desc = "Go to end of line" })
+	map("c", "<C-h>", "<Left>", { desc = "Move cursor left" })
+	map("c", "<C-l>", "<Right>", { desc = "Move cursor right" })
+	map("c", "<C-j>", "<Down>", { desc = "Next command in history" })
+	map("c", "<C-k>", "<Up>", { desc = "Previous command in history" })
+
+	-- Insert mode navigation
+	map("i", "<C-h>", "<Left>", { desc = "Move cursor left" })
+	map("i", "<C-l>", "<Right>", { desc = "Move cursor right" })
+	map("i", "<C-j>", "<Down>", { desc = "Move cursor down" })
+	map("i", "<C-k>", "<Up>", { desc = "Move cursor up" })
+
+	-- Quick save and quit
+	map("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
+	map("n", "<leader>W", "<cmd>wa<CR>", { desc = "Save all files" })
+	map("n", "<leader>q", "<cmd>q<CR>", { desc = "Quit" })
+	map("n", "<leader>Q", "<cmd>qa!<CR>", { desc = "Quit all without saving" })
+
+	-- File operations
+	map("n", "<leader>fn", "<cmd>enew<CR>", { desc = "New file" })
+	map("n", "<leader>fe", "<cmd>e!<CR>", { desc = "Reload file" })
+	map("n", "<leader>fR", "<cmd>e<CR>", { desc = "Refresh file" })
+
+	-- Text manipulation
+	map("n", "U", "<C-r>", { desc = "Redo" })
+	map("n", "<leader>J", "mzJ`z", { desc = "Join lines and restore cursor" })
+
+	-- Center screen on navigation
+	map("n", "<C-d>", "<C-d>zz", { desc = "Half page down and center" })
+	map("n", "<C-u>", "<C-u>zz", { desc = "Half page up and center" })
+	map("n", "n", "nzzzv", { desc = "Next search result and center" })
+	map("n", "N", "Nzzzv", { desc = "Previous search result and center" })
+
+	-- Select all
+	map("n", "<C-a>", "gg<S-v>G", { desc = "Select all" })
+
+	-- Increment/decrement numbers
+	map("n", "<leader>+", "<C-a>", { desc = "Increment number" })
+	map("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
+
+	-- Replace word under cursor
+	map(
+		"n",
+		"<leader>rw",
+		[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+		{ desc = "Replace word under cursor" }
+	)
+
+	-- Make file executable
+	map("n", "<leader>x", "<cmd>!chmod +x %<CR>", { desc = "Make file executable" })
+
+	-- Source current file
+	map("n", "<leader><leader>x", "<cmd>source %<CR>", { desc = "Source current file" })
+
+	-- Toggle options
+	map("n", "<leader>tw", "<cmd>set wrap!<CR>", { desc = "Toggle word wrap" })
+	map("n", "<leader>tn", "<cmd>set number!<CR>", { desc = "Toggle line numbers" })
+	map("n", "<leader>tr", "<cmd>set relativenumber!<CR>", { desc = "Toggle relative numbers" })
+	map("n", "<leader>ts", "<cmd>set spell!<CR>", { desc = "Toggle spell check" })
+
+	-- Macro shortcuts
+	map("n", "Q", "@q", { desc = "Execute macro q" })
+	map("v", "Q", ":norm @q<CR>", { desc = "Execute macro q on selection" })
 end
 
 function M.load_lsp_keymaps(bufnr)
