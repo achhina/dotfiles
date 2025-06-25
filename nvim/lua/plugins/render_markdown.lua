e7186bbbbb762cde5a914da7b1720e3dc573bf84
Adam Chhina <amanschhina@gmail.com>
2025-06-25 12:04:08 -0400

    Disable LaTeX support in render-markdown.nvim
    The latex2text executable is not available in the current texlive
    installation, causing health check warnings. Disable LaTeX support
    to avoid these warnings until latex2text can be properly installed.

--- nvim/lua/plugins/render_markdown.lua
+++ nvim/lua/plugins/render_markdown.lua
@@ -1,6 +1,9 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
-	opts = {},
+	opts = {
+		-- Disable LaTeX support since latex2text is not available
+		latex = { enabled = false },
+	},
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
 		"echasnovski/mini.nvim",
