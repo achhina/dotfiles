6683a7a614c4f3fa1533f7c1a2c5f6389aa51de7
Adam Chhina <amanschhina@gmail.com>
2025-06-28 07:40:04 -0400

    Configure render-markdown to use detex for LaTeX processing
    detex is already available from texlive.combined.scheme-medium and
    provides LaTeX to text conversion functionality equivalent to latex2text.

--- nvim/lua/plugins/render_markdown.lua
+++ nvim/lua/plugins/render_markdown.lua
@@ -1,6 +1,11 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
-	opts = {},
+	opts = {
+		-- Use detex instead of latex2text for LaTeX processing
+		latex = {
+			converter = "detex",
+		},
+	},
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
 		"echasnovski/mini.nvim",
