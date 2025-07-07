a505148ffdb0a809234ec14bdbeb296dc8e06aa9
Adam Chhina <amanschhina@gmail.com>
2025-07-07 09:00:05 -0400

    Add tmux Home Manager module
    Move tmux configuration from standalone tmux.conf to a declarative
    Home Manager module for better integration with the Nix-based
    dotfiles system.

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -6,6 +6,7 @@
     ./modules/git.nix
     ./modules/shell.nix
     ./modules/aerospace.nix
+    ./modules/tmux.nix
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
---
+++ nix/home-manager/modules/tmux.nix
@@ -0,0 +1,97 @@
+{ pkgs, ... }:
+
+{
+  programs.tmux = {
+    enable = true;
+    terminal = "screen-256color";
+    prefix = "C-a";
+    mouse = true;
+    escapeTime = 10;
+
+    extraConfig = ''
+      # Override ~/.tmux/plugins
+      setenv -g TMUX_PLUGIN_MANAGER_PATH "$XDG_CONFIG_HOME/tmux/plugins/"
+
+      # visual mode with v
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+
+      # Source tmux config
+      unbind r
+      bind r source-file $XDG_CONFIG_HOME/tmux/tmux.conf
+
+      # Uses VISUAL/EDITOR to determine key bindings but you can't use Esc to exit
+      # out of tmux command prompt because escape is mapped to switch mode
+      # https://github.com/tmux/tmux/issues/2426#issuecomment-711068362
+      set-option -g status-keys emacs
+      bind-key -T copy-mode-vi Escape send-keys -X cancel
+
+      # Open up scratch pad
+      bind-key -n M-g if-shell -F '#{==:#{session_name},popup}' {
+          set-option -t popup status off
+          detach-client
+      } {
+          display-popup -E "tmux new -A -s popup -t aichat"
+      }
+
+      # Status bar position
+      set-option -g status-position top
+
+      # Optimize nvim
+      set-option -g focus-events on
+      set-option -as terminal-overrides ",xterm-256color:RGB"
+
+      # Remove tmux-copycat plugin and just use tmux regex search
+      bind-key / copy-mode \; send-key ?
+
+      # Override tmux-pain-control
+      # <Prefix-l> to clear screen
+      bind l send-keys 'C-l'
+    '';
+
+    plugins = with pkgs.tmuxPlugins; [
+      {
+        plugin = tmux-mem-cpu-load;
+        extraConfig = "";
+      }
+      {
+        plugin = vim-tmux-navigator;
+        extraConfig = "";
+      }
+      {
+        plugin = yank;
+        extraConfig = "";
+      }
+      {
+        plugin = open;
+        extraConfig = ''
+          set -g @open-S 'https://www.google.com/search?q='
+        '';
+      }
+      {
+        plugin = sessionist;
+        extraConfig = "";
+      }
+      {
+        plugin = pain-control;
+        extraConfig = "";
+      }
+      {
+        plugin = tmux-powerline;
+        extraConfig = "";
+      }
+      {
+        plugin = resurrect;
+        extraConfig = ''
+          set -g @resurect-processes 'nvim'
+          set -g @resurrect-capture-pane-contents 'on'
+        '';
+      }
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+        '';
+      }
+    ];
+  };
+}
