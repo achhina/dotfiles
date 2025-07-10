1590fb7a45690133af50679b1eda4d599d11876b
Adam Chhina <amanschhina@gmail.com>
2025-07-10 15:36:57 -0400

    Use community caches

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -38,6 +38,21 @@
     JUPYTER_CONFIG_DIR = "${config.home.homeDirectory}/.config/jupyter";
   };
 
+  # Configure Nix settings
+  nix = {
+    package = pkgs.nix;
+    settings = {
+      substituters = [
+        "https://cache.nixos.org/"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+  };
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
