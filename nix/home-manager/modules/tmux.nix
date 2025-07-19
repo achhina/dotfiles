7a0928bb4b155f95f5a22fcd328144f06599f176
Adam Chhina <amanschhina@gmail.com>
2025-07-19 04:46:26 -0400

    Use fuzzy matching for neovim process restoration
    Change @resurrect-processes from 'nvim' to '"~nvim"' to handle complex Nix
    wrapper commands. The tilde syntax enables fuzzy matching to detect the full
    Nix store path but restore as simple 'nvim' command.

--- nix/home-manager/modules/tmux.nix
+++ nix/home-manager/modules/tmux.nix
@@ -228,7 +228,7 @@
       {
         plugin = resurrect;
         extraConfig = ''
-          set -g @resurrect-processes 'nvim'
+          set -g @resurrect-processes '"~nvim"'
           set -g @resurrect-capture-pane-contents 'on'
         '';
       }
