3121bc23b1a98968e56ac523092d3aafd583f717
Adam Chhina <amanschhina@gmail.com>
2025-09-03 23:11:11 +1000

    Disable Trouble's no-results warnings
    - Set warn_no_results = false in Trouble config
    - These warnings used Trouble's internal system (not vim.notify)
    - Couldn't show as Snacks toast popups anyway
    - Visual feedback from empty Trouble window is sufficient
    - Cleaner notification experience overall

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -5,6 +5,7 @@ return {
 	opts = {
 		focus = true, -- Focus the window when opened
 		follow = true, -- Follow the cursor
+		warn_no_results = false, -- Disable Trouble's internal warnings (they don't use vim.notify)
 		preview = {
 			type = "float",
 			relative = "editor",
