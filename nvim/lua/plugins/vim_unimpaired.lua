001ee6a54dfb4ff55a3d65fa6b39e26fa167453c
Adam Chhina <amanschhina@gmail.com>
2025-10-04 04:12:41 -0400

    Document expected which-key overlaps for vim-unimpaired
    Added comments explaining that overlap warnings for bracket mappings
    are expected behavior:
    - [y/]y (operator) with [yy/]yy (line encode/decode)
    - [x/]x (operator) with [xx/]xx (line XML encode/decode)
    - [u/]u (operator) with [uu/]uu (line URL encode/decode)
    - [C/]C (treesitter) with [CC/]CC (vim-unimpaired string encode/decode)
    
    Similar to marks.nvim and mini.comment, these overlaps are intentional -
    single letter mappings wait for motion/input, double letter mappings
    act immediately on the current line.

--- nvim/lua/plugins/vim_unimpaired.lua
+++ nvim/lua/plugins/vim_unimpaired.lua
@@ -1,3 +1,10 @@
 return {
+	-- Bracket mappings for common operations
+	-- Note: which-key will show overlap warnings for these mappings:
+	-- - [y/]y (operator) overlaps with [yy/]yy (encode/decode C string line)
+	-- - [x/]x (operator) overlaps with [xx/]xx (encode/decode XML line)
+	-- - [u/]u (operator) overlaps with [uu/]uu (encode/decode URL line)
+	-- - [C/]C (treesitter comment nav) overlaps with [CC/]CC (encode/decode string line)
+	-- These are expected - single letter waits for motion, double letter acts on line
 	"tpope/vim-unimpaired",
 }
