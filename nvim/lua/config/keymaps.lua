8a018b389dc1d9d1e20478f1e7c7f3643e4a72ca
Adam Chhina <amanschhina@gmail.com>
2025-10-18 18:59:07 -0400

    Fix terminal navigation by adding tmux-navigator terminal mode mappings
    The terminal navigation keymaps were using wincmd directly instead of
    TmuxNavigate commands, causing the literal command text to be inserted
    in terminal insert mode.
    
    Changes:
    - Add terminal mode keymaps to vim-tmux-navigator plugin config
    - Remove conflicting wincmd-based terminal keymaps from keymaps.lua
    - Keep leader+Esc for exiting terminal mode
    
    This ensures proper tmux/vim navigation integration in terminal buffers.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -174,10 +174,7 @@ function M.setup()
 
 	-- Terminal mode
 	safe_keymap("t", "<leader><Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
-	safe_keymap("t", "<C-h>", "<cmd>wincmd h<CR>", { desc = "Move to left window" })
-	safe_keymap("t", "<C-j>", "<cmd>wincmd j<CR>", { desc = "Move to lower window" })
-	safe_keymap("t", "<C-k>", "<cmd>wincmd k<CR>", { desc = "Move to upper window" })
-	safe_keymap("t", "<C-l>", "<cmd>wincmd l<CR>", { desc = "Move to right window" })
+	-- Terminal navigation handled by vim-tmux-navigator plugin
 
 	-- Options toggle (moved from <leader>t to avoid test conflicts)
 	safe_keymap("n", "<leader>ow", "<cmd>set wrap!<CR>", { desc = "Toggle word wrap" })
--- nvim/lua/plugins/vim_tmux_navigator.lua
+++ nvim/lua/plugins/vim_tmux_navigator.lua
@@ -4,11 +4,17 @@ return {
 	config = function()
 		vim.g.tmux_navigator_disable_when_zoomed = 1
 
-		-- Set up keymaps manually to ensure they work
+		-- Set up keymaps for normal mode
 		vim.keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>", { desc = "Navigate Left", silent = true })
 		vim.keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>", { desc = "Navigate Down", silent = true })
 		vim.keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>", { desc = "Navigate Up", silent = true })
 		vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", { desc = "Navigate Right", silent = true })
 		vim.keymap.set("n", "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", { desc = "Navigate Previous", silent = true })
+
+		-- Set up keymaps for terminal mode
+		vim.keymap.set("t", "<C-h>", "<cmd>TmuxNavigateLeft<cr>", { desc = "Navigate Left", silent = true })
+		vim.keymap.set("t", "<C-j>", "<cmd>TmuxNavigateDown<cr>", { desc = "Navigate Down", silent = true })
+		vim.keymap.set("t", "<C-k>", "<cmd>TmuxNavigateUp<cr>", { desc = "Navigate Up", silent = true })
+		vim.keymap.set("t", "<C-l>", "<cmd>TmuxNavigateRight<cr>", { desc = "Navigate Right", silent = true })
 	end,
 }
